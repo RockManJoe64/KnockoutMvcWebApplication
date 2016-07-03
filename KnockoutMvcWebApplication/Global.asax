153610065b31d7120ab8c948078c28468f9e4e7e
Jose E. Chavez <jose.e.chavez@gmail.com>
2016-07-02 21:15:29 -0500

    Add project files.

---
+++ KnockoutMvcWebApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KnockoutMvcWebApplication.MvcApplication" Language="C#" %>
---
+++ KnockoutMvcWebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KnockoutMvcWebApplication.Startup))]
+namespace KnockoutMvcWebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
